e9a72634750f47bf42dac0f524d2ae0e552f6e0c
adamascencio <ascencioadam@gmail.com>
2024-08-23 09:27:17 -0700

    chore: add body-parser middleware to parse incoming JSON payloads

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
