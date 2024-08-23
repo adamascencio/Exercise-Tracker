c2c21ce229e07575a6b916fc3e0f5c773f5d6f14
adamascencio <ascencioadam@gmail.com>
2024-08-23 09:48:42 -0700

    chore: reremove body-parser package for parsing JSON payloads

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
 const cors = require('cors')
 require('dotenv').config()
 require('./config/database')
 
 app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
