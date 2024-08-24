6f2b63d651885b466af6fb4002d29dd60de79f9f
adamascencio <ascencioadam@gmail.com>
2024-08-24 08:49:01 -0700

    fix: insert server middleware to append JSON request data to req.body

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('./config/database')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/users', require('./routes/api/users'))
 
