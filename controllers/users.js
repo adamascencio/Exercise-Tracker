1444e6a3280067c916b74de6b6c78316b0c117d2
adamascencio <ascencioadam@gmail.com>
2024-08-24 11:44:56 -0700

    feat: add API operation to return all users when for GET requests to /api/users

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,24 @@
 const User = require('../models/user');
 
-module.exports = { create }
+module.exports = { 
+  create,
+  getAllUsers
+ }
 
 async function create(req, res) {
   const request = { name: req.body.username };
   try {
     const user = await User.create(request);
     res.json({ data: user });
+  } catch (err) {
+    res.json({ data: err });
+  }
+}
+
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find({});
+    res.json({ data: users });
   } catch (err) {
     res.json({ data: err });
   }
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/users');
 
 router.post('/', usersCtrl.create);
+router.get('/', usersCtrl.getAllUsers);
 
 module.exports = router;
