09f3ac0497a755d220815c210dbd42309e332771
adamascencio <ascencioadam@gmail.com>
2024-08-24 07:37:37 -0700

    fix: add missing syntax to arrow function

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 
 module.exports = { create }
 
-const create = async (req, res) {
+const create = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.json({ data: user });
