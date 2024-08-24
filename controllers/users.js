8cb0d4c4a12bcd977f5caa52decc1d1817ab9a0f
adamascencio <ascencioadam@gmail.com>
2024-08-24 08:57:27 -0700

    fix: remove console.log

--- controllers/users.js
+++ controllers/users.js
@@ -2,9 +2,10 @@ const User = require('../models/user');
 
 module.exports = { create }
 
-const create = async (req, res) => {
+async function create(req, res) {
+  const request = { name: req.body.username };
   try {
-    const user = await User.create(req.body);
+    const user = await User.create(request);
     res.json({ data: user });
   } catch (err) {
     res.json({ data: err });
