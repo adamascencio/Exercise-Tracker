235fef8cd20008860cb637a4c9c55cfe789e0d04
adamascencio <ascencioadam@gmail.com>
2024-08-24 18:03:57 -0700

    fix: add document._id from newly created user to JSON response

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ async function create(req, res) {
   const request = { name: req.body.username };
   try {
     const user = await User.create(request);
-    res.json({ data: user });
+    res.json({ data: user, _id: user._id });
   } catch (err) {
     res.json({ data: err });
   }
