5d7083dec78c30e6d50cf4d769bb77f8433d859f
adamascencio <ascencioadam@gmail.com>
2024-08-29 09:32:48 -0700

    chore: remove unnecessary fields from mongoose schema during JSON response

--- models/user.js
+++ models/user.js
@@ -7,4 +7,15 @@ const userSchema = new Schema({
   timestamps: true
 });
 
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.username = ret.name;
+    delete ret.name;
+    delete ret.createdAt;
+    delete ret.updatedAt;
+    delete ret.__v;
+    return ret;
+  }
+});
+
 module.exports = mongoose.model('User', userSchema);
