b3ac8fa9c857a9c9b3bf2e8fdd338f426952ce29
adamascencio <ascencioadam@gmail.com>
2024-08-29 09:43:54 -0700

    feat: crrespond with an array of all users for GET /api/users requests

--- controllers/users.js
+++ controllers/users.js
@@ -18,12 +18,7 @@ async function create(req, res) {
 async function getAllUsers(req, res) {
   try {
     const users = await User.find({});
-    res.json(users.map(user => {
-      return {
-        username: user.name,
-        _id: user._id
-      }
-    }));
+    res.json(users);
   } catch (err) {
     res.json({ data: err });
   }
