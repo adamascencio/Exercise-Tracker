d33543dda01d79311850c7744c03d319a7d2a69a
adamascencio <ascencioadam@gmail.com>
2024-08-31 08:57:03 -0700

    feat: add password to user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  password: { type: String, required: true }
 }, {
   timestamps: true
 });
@@ -11,6 +12,7 @@ userSchema.set('toJSON', {
   transform: (doc, ret) => {
     ret.username = ret.name;
     delete ret.name;
+    delete ret.password;
     delete ret.createdAt;
     delete ret.updatedAt;
     delete ret.__v;
