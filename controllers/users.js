87d187b4f38dde82e9eeba85e0fd1d2518ad7676
adamascencio <ascencioadam@gmail.com>
2024-09-10 21:07:18 -0700

    chore: respond with jwt when new user is created

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 module.exports = { 
@@ -6,12 +8,15 @@ module.exports = {
  }
 
 async function create(req, res) {
-  const request = { name: req.body.username };
   try {
-    const user = await User.create(request);
-    res.json({ username: user.name, _id: user._id });
+    const { username, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = await User.create({ name: username, password: hash });
+    await user.save();
+    const token = jwt.sign({ username }, process.env.SECRET, { expiresIn: '24h' });
+    res.status(200).json({ token });
   } catch (err) {
-    res.json({ data: err });
+    res.status(500).json({ error: err });
   }
 }
 
