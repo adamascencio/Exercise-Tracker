9c6c8e8fea051de2da8840d3409fa18a59e7bdaa
adamascencio <ascencioadam@gmail.com>
2024-09-23 17:49:05 -0700

    chore: add username to the request object

--- config/checkToken.js
+++ config/checkToken.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.get('Authorization') || req.query.token;
-
-  if (token) {
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-      req.user = err ? null : decoded.user;
-      return next();
-    });
-  } else {
-    req.user = null;
-    return next();
-  }
-}
+  const token = req.header('Authorization');
+  
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+  
+  jwt.verify(token, process.env.SECRET, (err, decoded) => {
+    req.user = err ? null : decoded.username;
+    if (req.user) return next();
+    return res.status(400).json({ error: err });
+  });
+} 
