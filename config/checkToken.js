3798d12090c0f0a737386f661871809e5ff80bae
adamascencio <ascencioadam@gmail.com>
2024-10-26 10:19:39 -0700

    Fix: read token from url parameter instead of HTTP headers

--- config/checkToken.js
+++ config/checkToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.query.token;
   
   if (!token) return res.status(401).json({ error: 'Access denied' });
   
